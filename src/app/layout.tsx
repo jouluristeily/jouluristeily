8f5384e0f516b43d209517b38928e2872350d5d4
tokryr <topias.yrjola@gmail.com>
2023-09-21 19:25:05 +0300

    create footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import DrawerAppBar from '@/components/DrawerAppBar';
-import { Box } from '@mui/material';
+import { Box, Stack } from '@mui/material';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Footer from './Footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,8 +15,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.className}>
-        <DrawerAppBar />
-        <Box sx={{ px: { xs: 2, sm: 4, md: 10 }, py: 2 }}>{children}</Box>
+        <Stack justifyContent="space-between" minHeight="100vh" minWidth="100vw">
+          <DrawerAppBar />
+          <Box sx={{ px: { xs: 2, sm: 4, md: 10 }, py: 2, minHeight: '100vh' }}>{children}</Box>
+          <Footer />
+        </Stack>
       </body>
     </html>
   );
