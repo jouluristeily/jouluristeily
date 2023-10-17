da5ed52675b21ef6a1b34ecabcd5de7c02ed6fa6
tokryr <topias.yrjola@gmail.com>
2023-10-17 20:04:15 +0300

    add link to kide

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { Serialiser } from '@/components/Serialize';
 
 export default async function Home() {
   const content = await getContent();
-  const text = content.docs.find((doc: any) => doc.title === 'homepage')?.body;
 
   return (
     <>
@@ -18,7 +17,20 @@ export default async function Home() {
             <h3 className="text-3xl text-white mt-[-2rem]">30.11.-1.12.2023</h3>
           </div>
         </div>
-        <div className="p-4 md:p-10 text-center">{text && <Serialiser>{text}</Serialiser>}</div>
+        <h1
+          className={`center text-center text-4xl font-bold tracking-wide text-red-600 font-title`}
+        >
+          Tänä vuonna Jouluristeily seilataan 30.11.- 01.12.2023!
+        </h1>
+
+        <div className="flex justify-center items-center">
+          <a
+            href="#https://kide.app/events/f8092046-c026-4f4d-b735-d80d7498f865"
+            className="text-white text-2xl font-title bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-blue-red"
+          >
+            Lipunmyyntiin
+          </a>
+        </div>
       </div>
     </>
   );
