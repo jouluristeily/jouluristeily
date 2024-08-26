264cdce94748397d15167fa84b407efa45af4ae6
tokryr <topias.yrjola@gmail.com>
2024-08-26 18:13:56 +0300

    add keys to serializer

--- src/app/SerializeLexical.tsx
+++ src/app/SerializeLexical.tsx
@@ -6,63 +6,69 @@ interface SerializerProps {
 }
 
 const Serializer: FC<SerializerProps> = ({ content }) => {
-  const renderTextNode = (node: TextNode) => {
+  const renderTextNode = (node: TextNode, index: number) => {
     let className = '';
 
     if (node.format === 1) className += 'font-bold ';
     if (node.format === 2) className += 'italic ';
     if (node.format === 8) className += 'underline ';
 
-    return <span className={className.trim()}>{node.text}</span>;
+    return (
+      <span key={index} className={className.trim()}>
+        {node.text}
+      </span>
+    );
   };
 
-  const renderLineBreak = () => <br />;
+  const renderLineBreak = (index: number) => <br key={index} />;
 
-  const renderHeading = (node: HeadingNode) => {
+  const renderHeading = (node: HeadingNode, index: number) => {
     const Tag = node.tag;
     return (
-      <Tag>
-        {node.children.map((child, index) =>
-          child.type === 'text' ? renderTextNode(child) : renderLineBreak()
+      <Tag key={index}>
+        {node.children.map((child, childIndex) =>
+          child.type === 'text' ? renderTextNode(child, childIndex) : renderLineBreak(childIndex)
         )}
       </Tag>
     );
   };
 
-  const renderParagraph = (node: ParagraphNode) => {
+  const renderParagraph = (node: ParagraphNode, index: number) => {
     let className = '';
     if (node.format === 'center') className += 'text-center ';
     if (node.format === 'right') className += 'text-right ';
 
     return (
-      <p className={className.trim()}>
-        {node.children.map((child, index) =>
-          child.type === 'text' ? renderTextNode(child) : renderLineBreak()
+      <p key={index} className={className.trim()}>
+        {node.children.map((child, childIndex) =>
+          child.type === 'text' ? renderTextNode(child, childIndex) : renderLineBreak(childIndex)
         )}
-        {renderLineBreak()}
+        {renderLineBreak(node.children.length)} {/* Line break after the paragraph */}
       </p>
     );
   };
 
-  const renderList = (node: ListNode) => {
+  const renderList = (node: ListNode, index: number) => {
     const Tag = node.listType === 'bullet' ? 'ul' : 'ol';
     return (
-      <Tag>
-        {node.children.map((item, index) => (
-          <li key={index}>{item.children.map((child, childIndex) => renderTextNode(child))}</li>
+      <Tag key={index}>
+        {node.children.map((item, itemIndex) => (
+          <li key={itemIndex}>
+            {item.children.map((child, childIndex) => renderTextNode(child, childIndex))}
+          </li>
         ))}
       </Tag>
     );
   };
 
-  const renderNode = (node: ContentNode) => {
+  const renderNode = (node: ContentNode, index: number) => {
     switch (node.type) {
       case 'heading':
-        return renderHeading(node);
+        return renderHeading(node, index);
       case 'paragraph':
-        return renderParagraph(node);
+        return renderParagraph(node, index);
       case 'list':
-        return renderList(node);
+        return renderList(node, index);
       default:
         return null;
     }
@@ -71,7 +77,7 @@ const Serializer: FC<SerializerProps> = ({ content }) => {
   return (
     <div>
       {content.root.children.map((node, index) => (
-        <div key={index}>{renderNode(node)}</div>
+        <div key={index}>{renderNode(node, index)}</div>
       ))}
     </div>
   );
