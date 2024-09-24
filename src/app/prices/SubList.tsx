e9f8410c2e1806e3029e001b3d413ed6668bc538
tokryr <topias.yrjola@gmail.com>
2024-09-24 15:43:31 +0300

    add tuplis food prices

--- src/app/prices/SubList.tsx
+++ src/app/prices/SubList.tsx
@@ -1,10 +1,19 @@
 import PriceItem from './PriceItem';
 import { PriceListItem } from './page';
 
-export default function SubList({ title, hytit }: { title: string; hytit: PriceListItem[] }) {
+export default function SubList({
+  title,
+  hytit,
+  description,
+}: {
+  title: string;
+  hytit: PriceListItem[];
+  description?: string;
+}) {
   return (
     <div className="mt-8 text-center pb-8 min-w-full">
       <h2 className="text-2xl font-bold pb-3">{title}</h2>
+      {description && <p className="text-lg">{description}</p>}
       <div className="space-y-2">
         {hytit.map((item) => (
           <PriceItem
